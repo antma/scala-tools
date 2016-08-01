53b752c7465535dcbd193eb99664cbed80203cdd
Anton Maidel <anton.maidel@gmail.com>
2016-08-02 02:55:36 +0300

    TestWMMatching.scala: port Joris van Rantwijk's hand tests

--- TestWMMatching.scala
+++ TestWMMatching.scala
@@ -67,6 +67,7 @@ object Checker {
 }
 
 object TestWMMatching extends App {
+  val benchmark = 0
   def test(n: Int, min_weight: Int, max_weight: Int, seed: Int) {
     printf("test(n: %d, min_weight: %d, max_weight: %d, seed: %d)\n", n, min_weight, max_weight, seed)
     val r = new Random(seed)
@@ -93,22 +94,64 @@ object TestWMMatching extends App {
     assert(ra == 0)
   }
 
-  //test(300, 1, 10000, 2)
 
-  var seed = 1
-  val iterations = 1000
-  for (n <- 2 to 20 by 2 ;
-       t <- List(10, 20, 50, 100, 200, 500, 1000, 10000);
-       i <- 1 to iterations) {
-    test(n, 1, t, seed)
-    seed += 1
+  def handTests() : Unit = {
+    def check(edges:List[(Int,Int,Int)], maxcardinality: Boolean, expected: List[Int]) : Unit = {
+      assert(WMMatching.maxWeightMatching(edges, maxcardinality).toList == expected)
+    }
+
+    check(List((0,1,1)), false, List (1, 0))
+    check(List((1,2,10), (2,3,11)), false, List( -1, -1, 3, 2 ))
+    check(List((1,2,5), (2,3,11), (3,4,5)), false, List( -1, -1, 3, 2, -1))
+    check(List((1,2,5), (2,3,11), (3,4,5)), true, List(-1, 2, 1, 4, 3))
+    println("create S-blossom and use it for augmentation")
+    check(List((1,2,8), (1,3,9), (2,3,10), (3,4,7)), false, List( -1, 2, 1, 4, 3 ))
+    check(List((1,2,8), (1,3,9), (2,3,10), (3,4,7), (1,6,5), (4,5,6)), false, List( -1, 6, 3, 2, 5, 4, 1))
+    println("create S-blossom, relabel as T-blossom, use for augmentation")
+    check(List( (1,2,9), (1,3,8), (2,3,10), (1,4,5), (4,5,4), (1,6,3) ), false, List( -1, 6, 3, 2, 5, 4, 1 ))
+    check(List( (1,2,9), (1,3,8), (2,3,10), (1,4,5), (4,5,3), (1,6,4) ), false, List( -1, 6, 3, 2, 5, 4, 1 ))
+    check(List( (1,2,9), (1,3,8), (2,3,10), (1,4,5), (4,5,3), (3,6,4) ), false, List( -1, 2, 1, 6, 5, 4, 3 ))
+    println("create nested S-blossom, use for augmentation")
+    check(List( (1,2,9), (1,3,9), (2,3,10), (2,4,8), (3,5,8), (4,5,10), (5,6,6) ), false, List( -1, 3, 4, 1, 2, 6, 5 ))
+    println("create S-blossom, relabel as S, include in nested S-blossom")
+    check(List( (1,2,10), (1,7,10), (2,3,12), (3,4,20), (3,5,20), (4,5,25), (5,6,10), (6,7,10), (7,8,8) ), false, List( -1, 2, 1, 4, 3, 6, 5, 8, 7 ))
+    println("create nested S-blossom, augment, expand recursively")
+    check(List( (1,2,8), (1,3,8), (2,3,10), (2,4,12), (3,5,12), (4,5,14), (4,6,12), (5,7,12), (6,7,14), (7,8,12) ), false, List( -1, 2, 1, 5, 6, 3, 4, 8, 7 ))
+    println("create S-blossom, relabel as T, expand")
+    check(List( (1,2,23), (1,5,22), (1,6,15), (2,3,25), (3,4,22), (4,5,25), (4,8,14), (5,7,13) ), false, List( -1, 6, 3, 2, 8, 7, 1, 5, 4 ))
+    println("create nested S-blossom, relabel as T, expand")
+    check(List( (1,2,19), (1,3,20), (1,8,8), (2,3,25), (2,4,18), (3,5,18), (4,5,13), (4,7,7), (5,6,7) ), false, List( -1, 8, 3, 2, 7, 6, 5, 4, 1 ))
+    println("create blossom, relabel as T in more than one way, expand, augment")
+    check(List( (1,2,45), (1,5,45), (2,3,50), (3,4,45), (4,5,50), (1,6,30), (3,9,35), (4,8,35), (5,7,26), (9,10,5) ), false, List( -1, 6, 3, 2, 8, 7, 1, 5, 4, 10, 9 ))
+    println("again but slightly different")
+    check(List( (1,2,45), (1,5,45), (2,3,50), (3,4,45), (4,5,50), (1,6,30), (3,9,35), (4,8,26), (5,7,40), (9,10,5) ), false, List( -1, 6, 3, 2, 8, 7, 1, 5, 4, 10, 9 ))
+    // create blossom, relabel as T, expand such that a new least-slack S-to-free edge is produced, augment
+    check(List( (1,2,45), (1,5,45), (2,3,50), (3,4,45), (4,5,50), (1,6,30), (3,9,35), (4,8,28), (5,7,26), (9,10,5) ), false, List( -1, 6, 3, 2, 8, 7, 1, 5, 4, 10, 9 ))
+    println("create nested blossom, relabel as T in more than one way, expand outer blossom such that inner blossom ends up on an augmenting path")
+    check(List( (1,2,45), (1,7,45), (2,3,50), (3,4,45), (4,5,95), (4,6,94), (5,6,94), (6,7,50), (1,8,30), (3,11,35), (5,9,36), (7,10,26), (11,12,5) ), false, List( -1, 8, 3, 2, 6, 9, 4, 10, 1, 5, 7, 12, 11 ))
+    println("create nested S-blossom, relabel as S, expand recursively")
+    check(List( (1,2,40), (1,3,40), (2,3,60), (2,4,55), (3,5,55), (4,5,50), (1,8,15), (5,7,30), (7,6,10), (8,10,10), (4,9,30) ), false, List( -1, 2, 1, 5, 9, 3, 7, 6, 10, 4, 8 ))
   }
 
-  for (n <- 20 to 200 by 10;
-       t <- List(10, 20, 50, 100, 200, 500, 1000, 10000))  {
-    test(n, 1, t, seed)
-    seed += 1
-    testZeroMatching(n, seed)
-    seed += 1
+  if (benchmark > 0) {
+    test(300, 1, 10000, 2)
+  } else {
+    handTests()
+    var seed = 1
+    val iterations = 1000
+    for (n <- 2 to 20 by 2 ;
+         t <- List(10, 20, 50, 100, 200, 500, 1000, 10000);
+         i <- 1 to iterations) {
+      test(n, 1, t, seed)
+      seed += 1
+    }
+
+    for (n <- 20 to 200 by 10;
+         t <- List(10, 20, 50, 100, 200, 500, 1000, 10000))  {
+      test(n, 1, t, seed)
+      seed += 1
+      testZeroMatching(n, seed)
+      seed += 1
+    }
   }
 }
