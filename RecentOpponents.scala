dde4016bf8fc9324734cd4482a30a281e298849a
Anton Maidel <anton.maidel@gmail.com>
2016-08-17 23:38:17 +0300

    if score >= 1.0 then players just play together

--- RecentOpponents.scala
+++ RecentOpponents.scala
@@ -6,7 +6,7 @@ class RecentOpponents (m: Map[String, Double], s: Map[(String, String), Double])
       val s2 = m.get(u2).getOrElse(1.0)
       val x = s.get((u1, u2)).getOrElse(0.0)
       new RecentOpponents(m.updated(u1, s1 * 0.5).updated(u2, s2 * 0.5),
-                          s.updated((u1, u2), x + s1 * s2))
+                          s.updated((u1, u2), x + s1.max(s2)))
     }
   def score(u1: String, u2: String) : Double =
     if (u1 > u2) score(u2, u1)
