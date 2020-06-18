d5c3408e08771702fc5260f47fb180e9bb5c88ec
antma <antma@users.noreply.github.com>
2020-06-18 12:06:55 +0300

    return type CE fixes

--- src/test/scala/ArenaTournamentColorHistoryTest.scala
+++ src/test/scala/ArenaTournamentColorHistoryTest.scala
@@ -3,11 +3,14 @@ import scala.util.{Success, Failure}
 import org.specs2.mutable.Specification
 
 object ArenaTournamentColorHistoryTest {
-  def apply(s: String): (Int, Int) = {
-    val x = s.foldLeft(ArenaTournamentColorHistory(None)) { (acc, c) => c match {
+  def apply(s: String): ArenaTournamentColorHistory = {
+    s.foldLeft(ArenaTournamentColorHistory(None)) { (acc, c) => c match {
       case 'W' => acc.incColor(1)
       case 'B' => acc.incColor(-1)
     }}
+  }
+  def unpack(s: String): (Int, Int) = {
+    val x = apply(s)
     (x.strike, x.balance)
   }
 }
@@ -15,11 +18,11 @@ object ArenaTournamentColorHistoryTest {
 class ArenaTournamentColorHistoryTest extends Specification {
   "arena tournament color history" should {
      "hand tests" in {
-       ArenaTournamentColorHistoryTest("WWW") must_== (3, 3)
-       ArenaTournamentColorHistoryTest("WWWB") must_== (-1, 2)
-       ArenaTournamentColorHistoryTest("BBB") must_== (-3, -3)
-       ArenaTournamentColorHistoryTest("BBBW") must_== (1, -2)
-       ArenaTournamentColorHistoryTest("WWWBBB") must_== (-3, 0)
+       ArenaTournamentColorHistoryTest.unpack("WWW") must_== (3, 3)
+       ArenaTournamentColorHistoryTest.unpack("WWWB") must_== (-1, 2)
+       ArenaTournamentColorHistoryTest.unpack("BBB") must_== (-3, -3)
+       ArenaTournamentColorHistoryTest.unpack("BBBW") must_== (1, -2)
+       ArenaTournamentColorHistoryTest.unpack("WWWBBB") must_== (-3, 0)
     }
     "couldPlay" in {
       ArenaTournamentColorHistoryTest("WWW").couldPlay(
