0f7728619e827e059e2235513e7acb34855c68a9
antma <antma@users.noreply.github.com>
2020-06-18 14:35:28 +0300

    firstGetsWhite tests

--- src/test/scala/ArenaTournamentColorHistoryTest.scala
+++ src/test/scala/ArenaTournamentColorHistoryTest.scala
@@ -2,7 +2,7 @@ package lila.tournament
 import org.specs2.mutable.Specification
 
 object ArenaTournamentColorHistoryTest {
-  def apply(s: String): ArenaTournamentColorHistory = {
+  private def apply(s: String): ArenaTournamentColorHistory = {
     s.foldLeft(ArenaTournamentColorHistory(None)) { (acc, c) => c match {
       case 'W' => acc.incColor(1)
       case 'B' => acc.incColor(-1)
@@ -12,32 +12,37 @@ object ArenaTournamentColorHistoryTest {
     val x = apply(s)
     (x.strike, x.balance)
   }
+  def couldPlay(s1: String, s2: String, maxStreak: Int): Boolean = apply(s1).couldPlay(apply(s2), maxStreak)
+  def sameColors(s1: String, s2: String): Boolean = apply(s1).sameColors(apply(s2))
+  def firstGetsWhite(s1: String, s2: String): Boolean = apply(s1).firstGetsWhite(apply(s2))
 }
 
-//must be equalTo
-
 class ArenaTournamentColorHistoryTest extends Specification {
+  import ArenaTournamentColorHistoryTest.{ unpack, couldPlay, sameColors, firstGetsWhite }
   "arena tournament color history" should {
      "hand tests" in {
-       ArenaTournamentColorHistoryTest.unpack("WWW") must be equalTo((3, 3))
-       ArenaTournamentColorHistoryTest.unpack("WWWB") must be equalTo((-1, 2))
-       ArenaTournamentColorHistoryTest.unpack("BBB") must be equalTo((-3, -3))
-       ArenaTournamentColorHistoryTest.unpack("BBBW") must be equalTo((1, -2))
-       ArenaTournamentColorHistoryTest.unpack("WWWBBB") must be equalTo((-3, 0))
+       unpack("WWW") must be equalTo((3, 3))
+       unpack("WWWB") must be equalTo((-1, 2))
+       unpack("BBB") must be equalTo((-3, -3))
+       unpack("BBBW") must be equalTo((1, -2))
+       unpack("WWWBBB") must be equalTo((-3, 0))
     }
     "couldPlay" in {
-      ArenaTournamentColorHistoryTest("WWW").couldPlay(
-        ArenaTournamentColorHistoryTest("WWW"), 3) must beFalse
-      ArenaTournamentColorHistoryTest("BBB").couldPlay(
-        ArenaTournamentColorHistoryTest("BBB"), 3) must beFalse
-      ArenaTournamentColorHistoryTest("BB").couldPlay(
-        ArenaTournamentColorHistoryTest("BB"), 3) must beTrue
+      couldPlay("WWW", "WWW", 3) must beFalse
+      couldPlay("BBB", "BBB", 3) must beFalse
+      couldPlay("BB", "BB", 3) must beTrue
     }
     "sameColors" in {
-      ArenaTournamentColorHistoryTest("WWW").sameColors(
-        ArenaTournamentColorHistoryTest("W")) must beTrue
-      ArenaTournamentColorHistoryTest("B").sameColors(
-        ArenaTournamentColorHistoryTest("BBB")) must beTrue
+      sameColors("WWW", "W") must beTrue
+      sameColors("BBB", "B") must beTrue
+    }
+    "firstGetsWhite" in {
+      firstGetsWhite("WWW", "WW") must beFalse
+      firstGetsWhite("WW", "WWW") must beTrue
+      firstGetsWhite("BB", "B") must beTrue
+      firstGetsWhite("B", "BB") must beFalse
+      firstGetsWhite("WW", "BWW") must beFalse
+      firstGetsWhite("BB", "WBB") must beTrue
     }
   }
 }
